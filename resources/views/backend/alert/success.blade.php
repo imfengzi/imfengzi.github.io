3e53a31ae4f562f1948fc932f4907efcbb97b231
chaos <870579887@qq.com>
2021-04-14 16:28:15 +0800

    feat: test

--- resources/views/backend/alert/success.blade.php
+++
@@ -1,8 +0,0 @@
-@if(session('success'))
-    <div class="alert alert-success alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <i class="icon fa fa-check"></i>
-        提示消息!
-        <p>{{ session('success') }}</p>
-    </div>
-@endif
